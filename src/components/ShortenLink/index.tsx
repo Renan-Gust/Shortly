42efc4dfbc01d8fc53c7a710ac33a57e139469ac
Renan-Gust <renandevfullstack@gmail.com>
2021-12-23 17:59:16 -0300

    API integration

--- src/components/ShortenLink/index.tsx
+++ src/components/ShortenLink/index.tsx
@@ -1,9 +1,23 @@
 import { useEffect, useState, FormEvent } from 'react'
 import * as C from './styles'
 
+type linkGenerated = {
+
+}
+
 export function ShortenLink() {
     const [link, setLink] = useState('')
     const [isEmpty, setIsEmpty] = useState(false)
+    const [linkGenerated, setLinkGenerated] = useState<linkGenerated>({})
+
+    useEffect(() => {
+        (async () => {
+            const request = await fetch(`https://api.shrtco.de/v2/shorten?url=https://www.mtrek.com.br`)
+            const { ok, result } = await request.json()
+            console.log(ok, result)
+        })()
+    }, [])
+
 
     function handleSubmit(e: FormEvent) {
         e.preventDefault()
@@ -21,7 +35,8 @@ export function ShortenLink() {
         <C.Container>
             <C.Form onSubmit={handleSubmit}>
                 <C.Area>
-                    <C.TextInput 
+                    <C.TextInput
+                        type="url"
                         placeholder="Shorten a link here..." 
                         value={link}
                         onChange={e => setLink(e.target.value)}
